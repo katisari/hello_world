52476f582ea4b4b2291f8997c4692857595c1c29
Katie Lee <ktjlee@umich.edu>
2020-07-18 22:11:28 -0400

    initial

--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
   all_content = [];
+  API_KEY = "";
   constructor(private _http: HttpClient) {}
   // Session Services
   getSessions() {
@@ -31,6 +32,7 @@ export class HttpService {
     return this._http.delete(`/users/${id}/${sid}`);
   }
   getTranslation(input_words, source_lang, output_lang) {
-    return this._http.get(`https://translation.googleapis.com/language/translate/v2/?q=${input_words}&source=${source_lang}&target=${output_lang}&key=AIzaSyBelrl6Q_1scFMH6KBC5X0Pj2fjvb_mPdk`);
+    return this._http.get
+    (`https://translation.googleapis.com/language/translate/v2?target=${output_lang}&source=${source_lang}&key=${this.API_KEY}&q=${input_words}`);
   }
 }
